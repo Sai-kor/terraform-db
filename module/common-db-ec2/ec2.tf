a634bbe14d2803ab588778adae2e8d7f954e09b3
sai k <psai0073@gmail.com>
2022-03-16 17:27:11 -0400

    ec2 tag resource db-deploy

--- module/common-db-ec2/ec2.tf
+++ module/common-db-ec2/ec2.tf
@@ -9,3 +9,10 @@ resource "aws_spot_instance_request" "db" {
   wait_for_fulfillment = true
   vpc_security_group_ids = [aws_security_group.sg.id]
 }
+
+resource "aws_ec2_tag" "spot-instances" {
+  count = length(aws_spot_instance_request.db)
+  resource_id = aws_spot_instance_request.db.spot_instance_id[count.index]
+  key         = "Name"
+  value       = "${var.DB_COMPONENT}-${var.ENV}"
+}
